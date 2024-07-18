6c67f28f037457b7b6c4760debc548ff4a40051d
newrahul11 <you@example.com>
2024-07-18 16:53:04 +0530

    edit

---
+++ html/js/object.js
@@ -0,0 +1,16 @@
+let employee = [{
+    name: 'anuj',
+    age: 20,
+    designation: 'lead web developer',
+},
+{
+    name: 'jay',
+    age: 2,
+    designation: ' web developer',
+}];
+
+for(i=0;i<employee.length;i++){
+    let emp=employee[1];
+    console.log(emp.name);
+    
+}
