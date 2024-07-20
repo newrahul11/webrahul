67ae55653ee740652d116b37413452d1ab1636bd
newrahul11 <you@example.com>
2024-07-20 15:55:36 +0530

    edit

---
+++ html/js/index2.js
@@ -0,0 +1,23 @@
+let student = [{
+    name:"max",
+    age: 21,
+    strem:"it",
+    rollno: 10,
+    joining: 2004
+},
+{
+    name:"jack",
+    age: 18,
+    strem:"computer science",
+    rollno: 8,
+    joining: 2002
+},
+{
+    name:"anu",
+    age: 22,
+    strem:"inforamation technology",
+    rollno: 11,
+    joining: 2007
+},
+];
+
